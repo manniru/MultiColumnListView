bde0312be89aad59cb0e7a4bf564a882edc35611
MUHAMMAD MANNIR AHMAD <manniru@gmail.com>
2015-02-18 11:25:58 +0800

    Initialize source from Android Studio

---
+++ app/src/main/java/com/techlovejump/multicolumnlistview/MainActivity.java
@@ -0,0 +1,69 @@
+package com.techlovejump.multicolumnlistview;
+
+import static com.techlovejump.multicolumnlistview.Constants.FIRST_COLUMN;
+import static com.techlovejump.multicolumnlistview.Constants.FOURTH_COLUMN;
+import static com.techlovejump.multicolumnlistview.Constants.SECOND_COLUMN;
+import static com.techlovejump.multicolumnlistview.Constants.THIRD_COLUMN;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.mannir.multicolumnlistview.R;
+
+public class MainActivity extends Activity {
+
+    private ArrayList<HashMap<String, String>> list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView=(ListView)findViewById(R.id.listView1);
+
+        list=new ArrayList<HashMap<String,String>>();
+
+        HashMap<String,String> temp=new HashMap<String, String>();
+        temp.put(FIRST_COLUMN, "Ankit Karia");
+        temp.put(SECOND_COLUMN, "Male");
+        temp.put(THIRD_COLUMN, "22");
+        temp.put(FOURTH_COLUMN, "Unmarried");
+        list.add(temp);
+
+        HashMap<String,String> temp2=new HashMap<String, String>();
+        temp2.put(FIRST_COLUMN, "Rajat Ghai");
+        temp2.put(SECOND_COLUMN, "Male");
+        temp2.put(THIRD_COLUMN, "25");
+        temp2.put(FOURTH_COLUMN, "Unmarried");
+        list.add(temp2);
+
+        HashMap<String,String> temp3=new HashMap<String, String>();
+        temp3.put(FIRST_COLUMN, "Karina Kaif");
+        temp3.put(SECOND_COLUMN, "Female");
+        temp3.put(THIRD_COLUMN, "31");
+        temp3.put(FOURTH_COLUMN, "Unmarried");
+        list.add(temp3);
+
+        ListViewAdapter adapter=new ListViewAdapter(this, list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id)
+            {
+                int pos=position+1;
+                Toast.makeText(MainActivity.this, Integer.toString(pos)+" Clicked", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+    }
+}
